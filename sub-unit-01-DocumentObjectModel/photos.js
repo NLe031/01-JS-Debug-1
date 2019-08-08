55d4e544e4c689660f6cac39a9448010403d2d47
Nicholas Le <nle031@west-mec.org>
2019-08-07 17:25:45 -0700

    Added event listener
    added event listener from the first step of the second project.

--- sub-unit-01-DocumentObjectModel/photos.js
+++ sub-unit-01-DocumentObjectModel/photos.js
@@ -15,6 +15,7 @@ var photoOrder = [1,2,3,4,5];
 
 /* shift all images one figure to the left, and change values in photoOrder array to match  */
 function rightArrow() {
+   alert("left arrow clicked");
    for (var i = 0; i < 5; i++) {
       if ((photoOrder[i] + 1) === 6) {
          photoOrder[i] = 1;
@@ -39,14 +40,26 @@ function leftArrow() {
 
 /* open center figure in separate window */
 function zoomFig() {
+
+}
+/* will allow the computer to register the leftarrow click     */
+function createEventListeners() {
+   var leftarrow = document.getElementById("leftarrow");
+   if (leftarrow.addEventListener) {
+      leftarrow.addEventListener("click", leftArrow, true);
+   } 
+      else if (leftarrow.attachEvent) {
+         leftarrow.attachEvent("onclick", leftArrow);
+   }
    
 }
 
 /* create event listeners and populate image elements */
+
 function setUpPage() {
    createEventListeners();
    populateFigures();
-}
+} 
 
 /* run setUpPage() function when page finishes loading */
 if (window.addEventListener) {
