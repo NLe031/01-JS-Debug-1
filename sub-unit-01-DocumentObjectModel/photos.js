8a5a8e43bf9a989c491a933391534cf5df929fb2
Nicholas Le <nle031@west-mec.org>
2019-08-07 17:41:00 -0700

    added event listener
    added event listener from step two

--- sub-unit-01-DocumentObjectModel/photos.js
+++ sub-unit-01-DocumentObjectModel/photos.js
@@ -15,7 +15,7 @@ var photoOrder = [1,2,3,4,5];
 
 /* shift all images one figure to the left, and change values in photoOrder array to match  */
 function rightArrow() {
-   alert("left arrow clicked");
+   alert("right arrow clicked");
    for (var i = 0; i < 5; i++) {
       if ((photoOrder[i] + 1) === 6) {
          photoOrder[i] = 1;
@@ -28,6 +28,7 @@ function rightArrow() {
 
 /* shift all images one figure to the right, and change values in photoOrder array to match  */
 function leftArrow() {
+   alert("left arrow clicked");
    for (var i = 0; i < 5; i++) {
       if ((photoOrder[i] - 1) === 0) {
          photoOrder[i] = 5;
@@ -46,11 +47,26 @@ function zoomFig() {
 function createEventListeners() {
    var leftarrow = document.getElementById("leftarrow");
    if (leftarrow.addEventListener) {
-      leftarrow.addEventListener("click", leftArrow, true);
+      leftarrow.addEventListener("click", leftArrow, false);
    } 
       else if (leftarrow.attachEvent) {
          leftarrow.attachEvent("onclick", leftArrow);
    }
+   
+   var rightarrow = document.getElementById("rightarrow");
+   if (rightarrow.addEventListener) {
+      rightarrow.addEventListener("click", rightArrow, false);
+   } 
+      else if (rightarrow.attachEvent) {
+         rightarrow.attachEvent("onclick", rightArrow);
+   }
+
+   var mainFig = document.getElementsByTagName("img")[1];
+   if (mainFig.addEventListener) {
+      mainFig.addEventListener("click", zoomFig, false);
+  } else if (mainFig.attachEvent) {
+      mainFig.attachEvent("onclick", zoomFig);
+  }
    
 }
 
